009d7498eb66954250ffedb1c2d3641e18432579
Rafi5734 <kamrulhasun5734@gmail.com>
2024-03-12 11:07:39 +0600

    services section start

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import HeroSection from "./heroSection/HeroSection";
 import MainNavbar from "./navbar/MainNavbar";
+import Services from "./services/Services";
 
 const Main = () => {
   return (
     <div>
       <MainNavbar />
       <HeroSection />
+      <Services/>
     </div>
   );
 };
---
+++ src/components/main/services/Services.jsx
@@ -0,0 +1,22 @@
+const Services = () => {
+  return (
+    <div className="bg-[#f3f2f2] p-14">
+      <p className="text-center text-4xl font-bold">
+        Titas Tours & Travels Services
+      </p>
+      <p className="text-center mt-3 font-medium text-xl	">
+        Titas Tours & Travels is a full-service outbound and inbound air ticket
+        and travel agency in Bangladesh.
+      </p>
+      <div className="container mx-auto mt-10">
+        <div className="grid lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
